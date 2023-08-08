a66969e1d5d3883384c411ab2928ce9edc2e610f
Andrey <Dr_Wo1f@mail.ru>
2023-08-09 00:22:58 +0300

    so close to end

--- Main.java
+++ Main.java
@@ -2,18 +2,20 @@
 
 public class Main extends ToyShop{
     public static void main(String[] args) {
-        ToyShop shop = new ToyShop();
-        shop.showAllToys();
-        Toy mishka = new Toy("Мишка");
-        Toy car = new Toy("Машинка");
-        Toy kish= new Toy("Горшок");
-        Toy cat = new Toy("Кошечка");
-        Toy dog = new Toy("Собачка");
-        addToy(mishka);
-        addToy(car);
-        addToy(kish);
-        addToy(cat);
-        addToy(dog);
-        shop.showAllToys();
+        // ToyShop shop = new ToyShop();
+        // shop.showAllToys();
+        // Toy mishka = new Toy("Мишка");
+        // Toy car = new Toy("Машинка");
+        // Toy kish= new Toy("Горшок");
+        // Toy cat = new Toy("Кошечка");
+        // Toy dog = new Toy("Собачка");
+        // addToy(mishka);
+        // addToy(car);
+        // addToy(kish);
+        // addToy(cat);
+        // addToy(dog);
+        // shop.showAllToys();
+
+        Menu.menu();
     }
 }
--- Menu.java
+++ Menu.java
@@ -2,48 +2,48 @@ import java.util.Scanner;
 
 public class Menu {    
     
-    public boolean key = true;
-    Scanner scanner = new Scanner(System.in);
+    public static void menu() {
+        boolean key = true;
+        Scanner scanner = new Scanner(System.in);
+            
+        while(key){
+            System.out.print("""
+                Введите действие:
+                1. Добавление игрушки
+                2. Показать список игрушек
+                3. Розыгрыш игрушек
+                4. Изменения частоты выпадания (имя, новая вероятность)
+                0. Выход
+                \s""");
 
-    
-    while(key){
-        System.out.print("""
-            Введите действие:
-            1. Добавление игрушки
-            2. Показать список игрушек
-            3. Розыгрыш игрушек
-            4. Изменения частоты выпадания (имя, новая вероятность)
-
-            0. Выход
-            \s""");
-
-        int filter = scanner.nextInt();
-        
-        switch (filter){
-            case 1 -> {
-                System.out.println("Введите название игрушки");
-                String name = scanner.next();
-                Toy toy = new Toy(name);
+            int filter = scanner.nextInt();
+            
+            switch (filter){
+                case 1 -> {
+                    System.out.println("Введите название игрушки");
+                    String name = scanner.next();
+                    Toy toy = new Toy(name);
+                }   
+                case 2 -> {
+                    ToyShop.showAllToys();                
+                }            
+                case 3 -> {
+                    ToyShop.lottery();                
+                }
+                case 4 -> {
+                    String name = scanner.next();
+                    String probability = scanner.next();
+                    int probabilityInt = Integer.parseInt(probability);
+                    ToyShop.changeProbability(name, probabilityInt);                
+                }
+                case 0 -> {
+                    key = false;
+                }
+                // default -> {
+                //     // break;
+                //     key = false;
+                // }
             }   
-            case 2 -> {
-                ToyShop.showAllToys();                
-            }            
-            case 3 -> {
-                ToyShop.lottery();                
-            }
-            case 4 -> {
-                String name = scanner.next();
-                String probability = scanner.next();
-                int probabilityInt = Integer.parseInt(probability);
-                ToyShop.changeProbability(name, probabilityInt);                
-            }
-            case 0 -> {
-                key = false;
-            }
-            // default -> {
-            //     // break;
-            //     key = false;
-            // }
-        }   
-    } 
+        } 
+    }
 }
